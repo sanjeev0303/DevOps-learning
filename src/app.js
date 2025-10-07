319e0641b17200c80bd87a0ba7de20fb44fd4c08
sanjeev0303 <sanjeev.081021@gmail.com>
2025-10-07 22:15:45 +0530

    Implement ESLint and Prettier

--- src/app.js
+++ src/app.js
@@ -1,12 +1,9 @@
-import express from "express"
+import express from 'express';
 
+const app = express();
 
-const app = express()
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from DevOps');
+});
 
-
-app.get("/", (req, res) => {
-res.status(200).send("Hello from DevOps")
-})
-
-
-export default app
+export default app;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
-import app from "./app.js";
-import dotenv from "dotenv"
+import app from './app.js';
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
-
-app.listen(port , () => {
-    console.log(`Server is running on port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
